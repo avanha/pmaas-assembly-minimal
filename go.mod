3b8b01211557f06bb57aef5ff3ce93c02dfea287
Andre Vanha <andre@vanhas.com>
2026-02-13 14:19:33 -0700

    Minimal PMAAS assembly is now compiling and working

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/avanha/pmaas-assembly-minimal
 
 go 1.25.7
+
+require (
+	github.com/avanha/pmaas-core v0.0.1 // indirect
+	github.com/avanha/pmaas-spi v0.0.0 // indirect
+)
--- minimal_assembly.go
+++ minimal_assembly.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/avanha/pmaas-core"
+	"github.com/avanha/pmaas-core/config"
 )
 
 func main() {
 	fmt.Printf("Starting minimal PMAAS assembly\n")
+        conf := config.NewConfig()
+	conf.HttpPort = 8090
+        var pmaas = core.NewPMAAS(conf)
+	err := pmaas.Run()
+
+	if err != nil {
+		fmt.Printf("pmaas.Run completed with error: %s\n", err)
+	}
+
         fmt.Printf("Minimal PMAAS assembly terminstated\n")
 }
